0d24cb090c4e1b2b408fa12bc0f1968b581595e5
Juscelino Fernandes da Costa Junior <juscostajr@gmail.com>
2025-03-08 11:34:55 -0300

    retaforando validacao de data para uma abordagem orientada a objetos

--- validacaoData.ts
+++ validacaoData.ts
@@ -5,84 +5,93 @@
  */
 
 
-/*
- * Uma data válida consiste na validação do dia, mês e ano.
- */
-function validarData(dia: number, mes: number, ano:number): boolean {
-    const diaValido = validarDia(dia,mes,ano)
-    const mesValido = validarMes(mes)
-    const anoValido = validarAno(ano)
-    return diaValido && mesValido && anoValido
-}
+export class Data {
 
+    constructor(
+        private dia: number, 
+        private mes: number, 
+        private ano: number
+    ){}
 
-/*
-* O ano é considerado válido quando estiver no intervalo entre 1 e 9999.
-*/
-function validarAno(ano: number): boolean {
-    return ano >= 1 && ano <= 9999
-}
+    
+    /*
+    * Uma data válida consiste na validação do dia, mês e ano.
+    */
+    validarData(): boolean {
+        const diaValido = this.validarDia()
+        const mesValido = this.validarMes()
+        const anoValido = this.validarAno()
 
-
-/*
-* O mês é considerado válido quando estiver no intervalo entre 1 e 12.
-*/
-function validarMes(mes: number): boolean {
-    return mes >= 1 && mes <= 12
-}
-
-
-/*
-* O dia é considerado válido quando estiver no intervalo 
-* entre 1 e o número que representa a quantidade de dias do mês.
-*/
-function validarDia(dia: number, mes: number, ano:number): boolean {
-    const maximoDiasMes = obterQuantidadeDias(mes,ano)
-    return dia >= 1 && dia <= maximoDiasMes
-}
-
-
-/*
-* A quantidade de dias do mês varia conforme o mês, sendo que alguns tem 30 e outros têm 31.
-*/
-function obterQuantidadeDias(mes: number, ano: number): number {
-    const maximoDiasFevereiro = obterDiasFevereiro(ano)
-    const quantidadeDiasMes= {
-        1: 31,
-        2: maximoDiasFevereiro,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31
+        return diaValido && mesValido && anoValido
     }
-    return quantidadeDiasMes[mes]
+
+
+    /*
+    * O ano é considerado válido quando 
+    * estiver no intervalo entre 1 e 9999.
+    */
+    validarAno(): boolean {
+        return this.ano >= 1 && this.ano <= 9999
+    }
+
+
+    /*
+    * O mês é considerado válido quando estiver no intervalo entre 1 e 12.
+    */
+    validarMes(): boolean {
+        return this.mes >= 1 && this.mes <= 12
+    }
+
+
+    /*
+    * O dia é considerado válido quando estiver no intervalo 
+    * entre 1 e o número que representa a quantidade de dias do mês.
+    */
+    validarDia(): boolean {
+        const quantidadeDiasMes = this.obterQuantidadeDiasMes()
+
+        return this.dia >= 1 && this.dia <= quantidadeDiasMes
+    }
+
+
+    /*
+    * A quantidade de dias do mês varia conforme o mês, 
+    * sendo que alguns tem 30 e outros têm 31.
+    * Fevereiro é uma exceção, pois tem 29 dias em anos bissestos 
+    * e 28 dias nos demais anos.
+    */
+    obterQuantidadeDiasMes(): number {
+        const anoBissesto = this.validarAnoBissesto()
+        const quantidadeDiasMes = {
+            1: 31,
+            2: anoBissesto ? 29 : 28,
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31,
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31
+        }
+
+        return quantidadeDiasMes[this.mes]
+    }
+
+    
+    /*
+    * O ano bissesto pode ser identificado quando 
+    * for divisível por 4 e não for divisível por 100, 
+    * ou então quando for divisível por 400.
+    */
+    validarAnoBissesto(): boolean {
+        const divisivelPor4 = this.ano % 4 === 0
+        const divisivelPor100 = this.ano % 100 === 0
+        const divisivelPor400 = this.ano % 400 === 0
+
+        return (divisivelPor4 && !divisivelPor100) || divisivelPor400
+    }
 }
 
-
-/*
-* Fevereiro é uma exceção, pois tem 29 dias em anos bissestos 
-* e 28 dias nos demais anos.
-*/
-function obterDiasFevereiro(ano: number): number {
-    const anoBissesto = validarAnoBissesto(ano)
-    return anoBissesto ? 29 : 28
-}
-
-
-/*
-* O ano bissesto pode ser identificado quando 
-* for divisível por 4 e não for divisível por 100, 
-* ou então quando for divisível por 400.
-*/
-function validarAnoBissesto(ano: number): boolean {
-    const divisivelPor4 = ano % 4 === 0
-    const divisivelPor100 = ano % 100 === 0
-    const divisivelPor400 = ano % 400 === 0
-    return (divisivelPor4 && !divisivelPor100) || divisivelPor400
-}
